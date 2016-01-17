3fe8e0e972d23b083802b7f412accd3c8f051b50
Josh Zheng <denial@gmail.com>
2016-01-16 18:17:19 -0800

    removed arduino code.

--- run.py
+++ run.py
@@ -6,9 +6,6 @@ from dotenv import load_dotenv
 from watson_developer_cloud import SpeechToTextV1 as SpeechToText
 from watson_developer_cloud import AlchemyLanguageV1 as AlchemyLanguage
 
-import time
-import serial
-
 from speech_sentiment_python.recorder import Recorder
 
 def transcribe_audio(path_to_audio_file):
@@ -34,23 +31,7 @@ if __name__ == '__main__':
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
 
-    has_arduino = False
-    if len(sys.argv) > 1 and sys.argv[1] == 'arduino':
-        has_arduino = True
-
     recorder = Recorder("speech.wav")
-
-    if has_arduino:
-        # configure the serial connections (the parameters differs on the device you are connecting to)
-        ser = serial.Serial(
-            port='/dev/tty.usbmodem1411',
-            baudrate=9600,
-            parity=serial.PARITY_ODD,
-            stopbits=serial.STOPBITS_TWO,
-            bytesize=serial.SEVENBITS
-        )
-        ser.isOpen()
-        ser.flush()
 
     print("Please say something nice into the microphone\n")
     recorder.record_to_file()
@@ -63,13 +44,4 @@ if __name__ == '__main__':
     
     sentiment, score = get_text_sentiment(text)
     print(sentiment, score)
-
-    if has_arduino:
-        if float(score) > 0.4:
-            ser.write('p')
-        else:
-            ser.write('n')
-
-        ser.flush()
-        ser.close()
 
